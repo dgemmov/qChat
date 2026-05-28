b95ba48469a7731b39003570ea357625a8ad68cf
qualzed <klimpodogov365@gmail.com>
2026-05-28 12:46:46 +0300

    Added requirements.txt and base for cryptography

--- src/crypto/crypto_main.py
+++ src/crypto/crypto_main.py
@@ -1,1 +1,27 @@
+from cryptography.fernet import Fernet
+from src.crypto import key_generation
 # Main code of encryption and decryption will be soon ( now 10:00 PM 05.27.2026 )
+
+cipher = None
+
+def generateKey():
+     global cipher
+     key = Fernet.generate_key().decode()
+     cipher = Fernet(key.encode())
+     return key
+
+def returnEncrypted(data: str):
+    if cipher is None:
+        print("Cipher is not created!")
+        return None
+
+    encrypted_bytes = cipher.encrypt(data.encode())
+    return encrypted_bytes
+
+def returnDecrypted(data: bytes):
+    if cipher is None:
+        print("Cipher is not created!")
+        return None
+
+    decrypted_bytes = cipher.decrypt(data)
+    return decrypted_bytes.decode()
--- src/crypto/key_generation.py
+++ src/crypto/key_generation.py
@@ -1,2 +1,4 @@
+from src.crypto import crypto_main
+
 key_size: int = 0 # This value is changeble
-key: str = "" # Key which we will send when somebody connect
+key: str = crypto_main.generateKey() # Key which we will send when somebody connect
